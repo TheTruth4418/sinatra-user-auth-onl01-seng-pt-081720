f6e8e5be587d39c833ecd3714abaf0318cb9e522
William West <wwest466@gmail.com>
2020-12-16 17:59:31 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
-
-  create_table "Users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
